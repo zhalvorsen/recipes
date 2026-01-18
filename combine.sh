fe9473eb04e633069c6e1481239da260e1ade207
Zach Halvorsen <zhalvorsen@google.com>
2026-01-18 19:59:01 +0000

    Remove some extra stuff from the `combine.sh` script

--- combine.sh
+++ combine.sh
@@ -11,15 +11,6 @@ for dir in Breakfast Dessert Dinner Sauces Sides; do
   echo "" >> README.md
   # Loop through each markdown file in the directory, sorted alphabetically
   for file in $(ls "$dir"/*.md | sort); do
-    # Get the filename without the extension and directory
-    filename=$(basename -- "$file")
-    extension="${filename##*.}"
-    filename_no_ext="${filename%.*}"
-    # Replace underscores with spaces for the title
-    recipe_title=$(echo "$filename_no_ext" | tr '_' ' ')
-    # Add recipe title as a sub-heading
-    echo "### $recipe_title" >> README.md
-    echo "" >> README.md
     # Append the content of the file to README.md
     cat "$file" >> README.md
     # Add a newline for separation between recipes
